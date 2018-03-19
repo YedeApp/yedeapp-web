553f668051481d69d05f271f7fe34fdd39145439
Yede <yedeapp@163.com>
2018-03-19 22:30:50 +0800

    Add static login page also relevant styles

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,9 @@
 <header>
     <div class="container">
-        <a class="logo" href="">野得APP</a>
-        <div class="userinfo"></div>
+        <a class="logo" href="{{ route('home') }}"><img src="/images/yede_header_logo.png" alt="野得APP"></a>
+        <ul class="info">
+            <li><a href="{{ route('login') }}">登录</a></li>
+            <li><a href="{{ route('register') }}">注册</a></li>
+        </ul>
     </div>
 </header>
