19923e63fde865fd11180e82601bc3abacbadf16
Yede <yedeapp@163.com>
2018-03-21 14:37:52 +0800

    Resolve course header styles to a sass mixins

--- resources/views/course/chapters.blade.php
+++ resources/views/course/chapters.blade.php
@@ -4,9 +4,11 @@
 
 @section('content')
   @component('components.card')
-    <div class="chapters">
-      @isset($chapters)
-        <dl>
+    <div class="row p-md-2">
+      <div class="col-md-8"><h1>{{ $course->name }}</h1></div>
+      <div class="col-md-4"><div class="author">野得出品</div></div>
+      <div class="col-12">
+        <dl class="chapters">
           @foreach ($chapters as $chapter)
             <dt>{{ $chapter->name }}</dt>
             {{--  @foreach ($chapter->topics as $topic)
@@ -14,7 +16,7 @@
             @endforeach  --}}
           @endforeach
         </dl>
-      @endisset
+      </div>
     </div>
   @endcomponent
 @endsection
--- resources/views/course/show.blade.php
+++ resources/views/course/show.blade.php
@@ -3,29 +3,21 @@
 @section('title', $course->name)
 
 @section('content')
-<div class="container">
-  <div class="row">
-    <div class="col-12">
-      <div class="card">
-        <div class="card-body">
-          <div class="row">
-            <div class="col-md-8"><h1>{{ $course->name }}</h1></div>
-            <div class="col-md-4"><div class="author">野得出品</div></div>
-            <div class="col-12">
-              <div class="intro">{!! $course->introduction !!}</div>
-              <div class="extra">
-                <a class="btn btn-primary btn-w-120" href="{{ route('course.chapters', $course) }}">
-                  开始阅读
-                </a>
-                <a href="{{ route('course.purchase', $course) }}" class="btn btn-primary btn-w-120 ml-2">订阅 ￥{{ $course->getPriceForHumans() }}</a>
-              </div>
-            </div>
-          </div>
+  @component('components.card')
+    <div class="row p-md-2">
+      <div class="col-md-8"><h1>{{ $course->name }}</h1></div>
+      <div class="col-md-4"><div class="author">野得出品</div></div>
+      <div class="col-12">
+        <div class="intro">{!! $course->introduction !!}</div>
+        <div class="extra">
+          <a class="btn btn-primary btn-w-120" href="{{ route('course.chapters', $course) }}">
+            开始阅读
+          </a>
+          <a href="{{ route('course.purchase', $course) }}" class="btn btn-primary btn-w-120 ml-2">订阅 ￥{{ $course->getPriceForHumans() }}</a>
         </div>
       </div>
     </div>
-  </div>
-</div>
+  @endcomponent
 
 {{--  <div class="row course-detail">
     <div class="col-md-12 head">
