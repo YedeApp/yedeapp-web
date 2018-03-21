af76c0f6889afab5bd67a485d8c1c222baccb097
Yede <yedeapp@163.com>
2018-03-21 17:10:15 +0800

    List topics in chapters page

--- resources/views/course/chapters.blade.php
+++ resources/views/course/chapters.blade.php
@@ -9,7 +9,7 @@
       <div class="col-md-4"><div class="author">野得出品</div></div>
       <div class="col-12">
         <div class="extra">
-          <a href="{{ route('course.purchase', $course) }}" class="btn btn-primary btn-w-120 ml-2">订阅 ￥{{ $course->getPriceForHumans() }}</a>
+          <a href="{{ route('course.purchase', $course) }}" class="btn btn-primary btn-w-150">订阅 ￥{{ $course->getPriceForHumans() }}</a>
         </div>
       </div>
       <div class="col-12">
@@ -17,11 +17,21 @@
           @foreach ($chapters as $chapter)
             <dt>{{ $chapter->name }}</dt>
             @foreach ($chapter->topics as $topic)
-              <dd>{{ $topic->title }}</dd>
+              <dd>
+                {{--subscriber  <a href="#">{{ $topic->title }}</a>  --}}
+                <a href="#"><span class="badge badge-primary">免费试读</span>{{ $topic->title }}</a>
+                <span class="pull-right">
+                  <svg class="icon" aria-hidden="true" title="订阅后开启"><use xlink:href="#icon-lock"></use></svg>
+                </span>
+              </dd>
             @endforeach
           @endforeach
         </dl>
       </div>
     </div>
   @endcomponent
+@endsection
+
+@section('scripts')
+<script src="{{ asset('js/iconfont.js') }}"></script>
 @endsection
