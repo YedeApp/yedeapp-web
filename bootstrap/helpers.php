bfcc0bfc9a3f1057a76e752be41c0bdbc9fa4be7
Yede <yedeapp@163.com>
2018-04-19 21:16:50 +0800

    Add hieu-le/active package then set user nav active class

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Produce a css class by page's route name
+ */
 function getRouteCls()
 {
     $routeClsName = str_replace('.', '-', Route::currentRouteName());
@@ -7,6 +10,9 @@ function getRouteCls()
     return $routeClsName;
 }
 
+/**
+ * External link
+ */
 function outlink($href, $text, $target = 'blank')
 {
     $rel = '';
@@ -18,6 +24,9 @@ function outlink($href, $text, $target = 'blank')
     return "<a href=\"{$href}\" target=\"_{$target}\" {$rel}>{$text}</a>";
 }
 
+/**
+ * Truncate strings to defined length.
+ */
 function truncate($value, $length = 200)
 {
     $string = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
@@ -25,6 +34,27 @@ function truncate($value, $length = 200)
     return str_limit($string, $length);
 }
 
+/**
+ * Jump to specific comment location.
+ */
 function jumpToComment($comment, $courseSlug = '') {
     return $comment->topic->link($courseSlug) . '#comment_' . $comment->id;
+}
+
+/**
+ * Check if the current tab is actived by url params.
+ */
+function checkNavActive($if_route, $if_route_param) {
+    return active_class($if_route && $if_route_param);
+}
+
+/**
+ * Check nav active status on user profile page.
+ */
+function checkUserNavActive($value = '', $key = 'tab') {
+    $if_route = if_route('user.show');
+
+    $if_route_param = if_route_param($key, $value);
+
+    return checkNavActive($if_route, $if_route_param);
 }
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
   @component('components.card')
-    <div class="row profile">
+    <div class="row ">
       <div class="col-md-3 text-center">
         <img id="avatar-image" src="{{ $user->avatar }}" alt="{{ $user->name }}" class="img-thumbnail img-avatar" width="188">
       </div>
@@ -12,17 +12,19 @@
         <div class="name">{{ $user->name }}</div>
         <div class="intro">{{ truncate($user->introduction, 120) }}</div>
       </div>
+      @can('update', $user)
       <div class="col-md-2 align-self-end text-right">
         <a href="{{ route('user.edit', $user->id) }}" class="btn btn-outline-primary">编辑个人资料</a>
       </div>
+      @endcan
     </div>
   @endcomponent
 
   @component('components.card')
     <nav>
       <div class="nav nav-tabs" id="nav-tab" role="tablist">
-        <a class="nav-item nav-link active" href="{{ route('user.show', [$user, 'activities']) }}">动态</a>
-        <a class="nav-item nav-link" href="{{ route('user.show', [$user, 'comments']) }}">留言</a>
+        <a class="nav-item nav-link {{ active_class( if_route('user.show') && (if_route_param('tab', '') || if_route_param('tab', 'activities')) ) }}" href="{{ route('user.show', [$user, 'activities']) }}">动态</a>
+        <a class="nav-item nav-link {{ active_class( if_route('user.show') && if_route_param('tab', 'comments') ) }}" href="{{ route('user.show', [$user, 'comments']) }}">留言</a>
       </div>
     </nav>
     <div class="tab-content">
