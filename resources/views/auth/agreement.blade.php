8647ebca702a05348666547bb1dbcbf1302ff045
Yede <yedeapp@163.com>
2018-05-04 14:21:02 +0800

    Add agreement page

---
+++ resources/views/auth/agreement.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>野得App服务协议 - {{ config('app.name') }}</title>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+  {{-- No support for IE which version less than 10 --}}
+  <!--[if lt IE 10]>
+  <div class="alert alert-warning alert-dismissible fade show m-0 rounded-0" role="alert">
+    您目前使用的 IE 浏览器版本过于陈旧。请使用现代的浏览器以获得最佳的浏览效果。
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <![endif]-->
+
+  <div id="app" class="{{ getRouteCls() }}-page">
+    <div class="container">
+      <div class="zh-terms-content zh-document">
+        <h1>野得App服务协议</h1>
+        <p>欢迎您使用野得App。</p>
+        <p>
+          请您仔细阅读以下条款，如果您对本协议的任何条款表示异议，您可以选择不使用野得App。当您注册成功，无论是进入野得App，还是在野得App上浏览和发布任何内容（即「内容」），均意味着您（即「用户」）完全接受本协议项下的全部条款。
+        </p>
+        <div id="section-rule" data-section="sec-rule">
+          <a name="sec-rule" class="zg-anchor-hidden"></a>
+          <h2>使用规则</h2>
+          <ol>
+            <li>用户注册成功后，野得App将给予每个用户一个用户帐号，该用户帐号和密码由用户负责保管；用户应当对其用户帐号进行的所有活动、事件以及帐户安全行为负责。</li>
+            <li>用户须对在野得App的注册信息的真实性、合法性、有效性承担全部责任，用户不得冒充他人；不得利用他人的名义发布任何信息；不得恶意使用注册帐号导致其他用户误认；对恶意使用帐号的用户，野得App有权立即停止提供服务，收回其帐号并由用户独自承担由此而产生的一切责任。</li>
+            <li>用户直接或通过各类方式间接使用野得App服务和数据的行为，都将被视作已无条件接受本协议全部内容；若用户对本协议的任何条款存在异议，请停止使用野得App所提供的全部服务。</li>
+            <li>用户承诺不得以任何方式利用野得App直接或间接从事违反中国法律以及社会公德的行为，野得App有权对违反上述承诺的内容予以删除。</li>
+            <li>
+              <p>用户不得利用野得App服务制作、上载、复制、发布、传播或者转载如下内容：</p>
+              <ul>
+                <li>反对宪法所确定的基本原则的；</li>
+                <li>危害国家安全，泄露国家秘密，颠覆国家政权，破坏国家统一的；</li>
+                <li>损害国家荣誉和利益的；</li>
+                <li>煽动民族仇恨、民族歧视，破坏民族团结的；</li>
+                <li>破坏国家宗教政策，宣扬邪教和封建迷信的；</li>
+                <li>散布谣言，扰乱社会秩序，破坏社会稳定的；</li>
+                <li>散布淫秽、色情、赌博、暴力、凶杀、恐怖或者教唆犯罪的；</li>
+                <li>侮辱或者诽谤他人，侵害他人合法权益的；</li>
+                <li>含有法律、行政法规禁止的其他内容的信息。</li>
+              </ul>
+            </li>
+            <li>用户承诺不会以任何技术或方式攻击或破坏或改变野得App的全部或部分、或干扰其运行；不会以任何非法方式获取或使用野得App的任何软件、代码或其他技术或信息；不会对野得App运行的任何程序进行反向工程、反向编译、反向汇编或改写。</li>
+            <li>野得App有权对用户使用野得App的情况进行审查和监督，如用户在使用野得App时违反任何上述规定，野得App或其授权的人有权要求用户改正或直接采取一切必要的措施（包括但不限于更改或删除用户张贴的内容、暂停或终止用户使用野得App的权利）以减轻用户不当行为造成的影响。</li>
+          </ol>
+        </div>
+        <div id="section-licence" data-section="sec-licence">
+          <a name="sec-licence" class="zg-anchor-hidden"></a>
+          <h2>服务方式</h2>
+          <ol>
+            <li data-section="sec-licence-1">
+              <a name="sec-licence-1" class="zg-anchor-hidden"></a>
+              野得App是网络内容服务提供者，可针对不同传播渠道用户为其提供内容服务。用户应当尊重野得App创作内容服务的付出和劳动，认识到保护知识产权对野得App生存与发展的重要性。用户承诺除了野得App明文允许的内容以外，不得将野得App的内容服务通过任何方法或手段泄露到任何的社交媒体、社区和网络平台。否则野得App有权立即停止提供服务，收回其帐号并由用户承担野得App的损失，以及由此而产生的一切法律责任。
+            </li>
+            <li data-section="sec-licence-2">
+              <a name="sec-licence-2" class="zg-anchor-hidden"></a>
+              野得App提供的网络服务中包含的标识、版面设计、排版方式、文本、图片、图形等均受著作权、商标权及其它法律保护，未经相关权利人（含野得App及其他原始权利人）同意，上述内容均不得在任何平台被直接或间接发布、使用、出于发布或使用目的的改写或再发行，或被用于其他任何商业目的。
+            </li>
+            <li data-section="sec-licence-3">
+              <a name="sec-licence-3" class="zg-anchor-hidden"></a>
+              野得App上可由多人参与编辑的内容，包括但不限于评论、补充、说明、总结、描述、话题，所有参与编辑者均同意，相关知识产权归野得App所有。
+            </li>
+            <li data-section="sec-licence-4">
+              <a name="sec-licence-4" class="zg-anchor-hidden"></a>
+              为了更好提供知识服务，野得App内容服务可能提供给用户捐赠或赞赏的页面，参与捐赠或赞赏的用户可获得更多或更优质的内容服务，但捐赠和赞赏属于用户自发行为，用户不得因为服务内容未满足个人需求而要求返还捐赠和赞赏款项。
+            </li>
+            <li data-section="sec-licence-5">
+              <a name="sec-licence-5" class="zg-anchor-hidden"></a>
+              用户应当了解野得App可能对内容服务进行更改、更新或提高，用户承诺不因野得App更改、更新或提高其服务给用户造成的损失和不便而向野得App要求赔偿或补偿。
+            </li>
+          </ol>
+        </div>
+        <div id="section-privacy" data-section="sec-privacy">
+          <a name="sec-privacy" class="zg-anchor-hidden"></a>
+          <h2>个人隐私</h2>
+          <ol>
+            <li>尊重用户个人隐私信息的私有性是野得App的一贯原则，野得App将通过技术手段、强化管理等办法充分保护用户的个人隐私信息，除法律或有法律赋予权限的政府部门要求或事先得到用户明确授权等原因外，野得App保证不对外公开或向第三方透露用户个人隐私信息，或用户在使用服务时存储的非公开内容。</li>
+            <li>同时，为了运营和改善野得App的技术与服务，野得App将可能会自行收集使用或向第三方提供用户的非个人隐私信息，这将有助于野得App向用户提供更好的用户体验和服务质量。</li>
+          </ol>
+        </div>
+        <div id="section-impunity" data-section="sec-impunity">
+          <a name="sec-impunity" class="zg-anchor-hidden"></a>
+          <h2>免责申明</h2>
+          <ol>
+            <li>野得App尽可能为用户提供优质的内容服务，但用户应了解野得App不对内容服务的时效性和正确性进行保证。</li>
+            <li>用户在野得App发表的内容仅表明其个人的立场和观点，并不代表野得App的立场或观点。作为内容的发表者，需自行对所发表内容负责，因所发表内容引发的一切纠纷，由该内容的发表者承担全部法律及连带责任。野得App不承担任何法律及连带责任。</li>
+            <li>野得App不保证网络服务一定能满足用户的要求，也不保证网络服务不会中断，对网络服务的及时性、安全性、准确性也都不作保证。</li>
+            <li>对于因不可抗力或野得App不能控制的原因造成的网络服务中断或其它缺陷，野得App不承担任何责任，但将尽力减少因此而给用户造成的损失和影响。</li>
+          </ol>
+        </div>
+        <div id="section-modification" data-section="sec-modification" style="">
+          <a name="sec-modification" class="zg-anchor-hidden"></a>
+          <h2>协议修改</h2>
+          <ol>
+            <li>根据互联网的发展和有关法律、法规及规范性文件的变化，或者因业务发展需要，野得App有权对本协议的条款作出修改或变更，一旦本协议的内容发生变动，野得App将会直接在野得App网站上公布修改之后的协议内容，该公布行为视为野得App已经通知用户修改内容。野得App也可采用电子邮件或私信的传送方式，提示用户协议条款的修改、服务变更、或其它重要事项。</li>
+            <li>如果不同意野得App对本协议相关条款所做的修改，用户应当立即停止使用野得App。如果用户继续使用野得App，则视为用户接受野得App对本协议相关条款所做的修改。</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
