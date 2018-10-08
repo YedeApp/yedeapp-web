69f674636c93e5c61e24187268850b75bf1a982d
Yede <yedeapp@163.com>
2018-10-08 23:46:29 +0800

    adjust prev and next in article detail page

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -31,8 +31,8 @@ class TopicController extends Controller
     public function show(Request $request, Course $course, Topic $topic)
     {
         // Get previous and next topic
-        $prev = Topic::where('id', '<', $topic->id)->where('course_id', $topic->course_id)->where('chapter_id', $topic->chapter_id)->orderBy('sorting', 'desc')->orderBy('id', 'desc')->first();
-        $next = Topic::where('id', '>', $topic->id)->where('course_id', $topic->course_id)->where('chapter_id', $topic->chapter_id)->orderBy('sorting', 'asc')->orderBy('id', 'asc')->first();
+        $prev = Topic::where('sorting', '<', $topic->sorting)->where('course_id', $topic->course_id)->where('chapter_id', $topic->chapter_id)->orderBy('sorting', 'desc')->orderBy('id', 'desc')->first();
+        $next = Topic::where('sorting', '>', $topic->sorting)->where('course_id', $topic->course_id)->where('chapter_id', $topic->chapter_id)->orderBy('sorting', 'asc')->orderBy('id', 'asc')->first();
 
         // Get comments and replies.
 		$comments = $topic->comments()->where('parent_id', null)->orderBy('likes', 'desc')->orderBy('created_at', 'desc')->get()->load('user');
